cd37f7dae4ef0e743b8b13a8dc88c16ed97999cc
Felipe Alonso <xfelipe98@gmail.com>
2023-03-27 10:14:28 -0500

    Last change: Corrected UserFilter

--- Domino.Api/Filters/UserFilter.cs
+++ Domino.Api/Filters/UserFilter.cs
@@ -1,6 +1,7 @@
 ﻿using Domino.Api.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
 
 namespace Domino.Api.Filters;
 
@@ -10,16 +11,27 @@ public class UserFilter : Attribute, IActionFilter
     {
         string method = context.ActionDescriptor.DisplayName!;
 
-
         if (context.Result is OkObjectResult result && result.Value is null)
         {
             if (method.Contains(nameof(UserController.LogIn)))
             {
-                context.Result = new NotFoundObjectResult("Incorrect UserName or Password");
+                context.Result = new NotFoundObjectResult(new ProblemDetails
+                { 
+                    Type = "https://www.rfc-editor.org/rfc/rfc7231#section-6.5.4",
+                    Title = "Incorrect Payload.",
+                    Status = (int)HttpStatusCode.NotFound,
+                    Detail = "Incorrect UserName or Password."
+                });
             }
             if (method.Contains(nameof(UserController.SignUp)))
             {
-                context.Result = new ConflictObjectResult("User already exists");
+                context.Result = new ConflictObjectResult(new ProblemDetails
+                {
+                    Type = "https://www.rfc-editor.org/rfc/rfc7231#section-6.5.8",
+                    Title = "Incorrect Payload.",
+                    Status = (int)HttpStatusCode.Conflict,
+                    Detail = "User already exists."
+                });
             }
         }
     }
